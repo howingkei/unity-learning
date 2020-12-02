aa4636970475de13c089135f5b2afc1d752ba1c1
howingkei <949247033@qq.com>
2020-12-02 17:27:51 +0800

    hw8

---
+++ hw8/Assets/Particle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle : MonoBehaviour
+{
+    public float engineRevs;
+    public float exhaustRate;
+
+    ParticleSystem exhaust;
+    // Start is called before the first frame update
+    void Start()
+    {
+        exhaust = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        exhaust.emissionRate = engineRevs * exhaustRate;
+        engineRevs = engineRevs + 10;
+    }
+}
