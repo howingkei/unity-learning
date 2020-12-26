4833a6711f8d851da3ea737d67ef61805c437b62
howingkei <949247033@qq.com>
2020-12-26 17:03:33 +0800

    hw9

---
+++ HealthBar/Assets/Scripts/IMGUI.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IMGUI : MonoBehaviour
+{
+    // 最大血量
+    public float MaxValue = 10.0f;
+    // 当前血量
+    public float CurValue;
+    private float LerpValue;
+    public Slider healthSlider;
+    
+    private void Start()
+    {
+        // 默认最大血量是10.0，可以调整
+        MaxValue = 10.0f;
+        // 初始血量为1.0
+        CurValue = 1.0f;
+        LerpValue = 1.0f;
+        healthSlider.value = CurValue;
+    }
+    void OnGUI()
+    {
+        GUI.HorizontalScrollbar(new Rect(25, 25, 300, 50), 0.0f, CurValue, 0.0f, MaxValue);
+
+        if (GUI.Button(new Rect(45, 90, 50, 30), "加血"))
+        {
+            LerpValue += 1.0f;
+            if (LerpValue > 10.0f)
+            {
+                LerpValue = 10.0f;
+            }
+        }
+        if (GUI.Button(new Rect(225, 90, 50, 30), "减血"))
+        {
+            LerpValue -= 1.0f;
+            if (LerpValue < 0.0f)
+            {
+                LerpValue = 0.0f;
+            }
+        }
+        CurValue = Mathf.Lerp(CurValue, LerpValue, 0.05f);
+        healthSlider.value = CurValue;
+    }
+}
---
+++ HealthBar/README.md
@@ -0,0 +1,1 @@
+博客地址：https://blog.csdn.net/qq_40338696/article/details/111753353
